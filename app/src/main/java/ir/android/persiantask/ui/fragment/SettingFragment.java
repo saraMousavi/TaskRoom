efb7ec975cf1052cc4993f704eebc35c2b3a36bd
ASUS <sara.mousavi.90@gmail.com>
2020-09-02 16:11:58 +0430

    Setting Fragment

--- app/src/main/java/ir/android/persiantask/ui/fragment/SettingFragment.java
+++ app/src/main/java/ir/android/persiantask/ui/fragment/SettingFragment.java
@@ -9,6 +9,9 @@ import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
 import org.jetbrains.annotations.NotNull;
 
 import ir.android.persiantask.R;
@@ -18,13 +21,48 @@ public class SettingFragment extends Fragment {
 
     private static final String ARG_TITLE = "arg_title";
     private static final String ARG_BG_COLOR = "arg_bg_color";
+    private CollapsingToolbarLayout toolBarLayout;
+    private View inflatedView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.setting_fragment, container, false);
+
+        View inflatedView = inflater.inflate(R.layout.setting_fragment, container, false);
+        this.inflatedView = inflatedView;
+
+        return inflatedView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        init();
+        AppBarLayout mAppBarLayout = (AppBarLayout) this.inflatedView.findViewById(R.id.app_bar);
+        mAppBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = false;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    toolBarLayout.setTitle(getString(R.string.Setting));
+                    isShow = true;
+                } else if (isShow) {
+                    toolBarLayout.setTitle(" ");
+                    isShow = false;
+                }
+            }
+        });
+    }
+
+    private void init() {
+        toolBarLayout = (CollapsingToolbarLayout) this.inflatedView.findViewById(R.id.toolbar_layout);
     }
 
     @JvmStatic
