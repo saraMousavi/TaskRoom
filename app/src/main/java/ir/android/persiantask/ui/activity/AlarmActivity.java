d5646dcec9f11fe764311b78680755bab79e90d0
ASUS <sara.mousavi.90@gmail.com>
2020-09-20 18:25:16 +0430

    improve ui of alarm activity

--- app/src/main/java/ir/android/persiantask/ui/activity/AlarmActivity.java
+++ app/src/main/java/ir/android/persiantask/ui/activity/AlarmActivity.java
@@ -12,12 +12,17 @@ import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import java.util.List;
 
@@ -26,27 +31,38 @@ import ir.android.persiantask.ui.activity.reminder.AddEditReminderActivity;
 import ir.android.persiantask.ui.services.AlarmJobService;
 
 public class AlarmActivity extends AppCompatActivity {
-    private TextView helloAlarm;
-    private JobScheduler mScheduler;
-    private Intent stopIntent;
-    private SharedPreferences sharedPreferences;
+    private ImageView alarmIcon;
+    private SwitchCompat alarmActive;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.alarm_activity);
-        helloAlarm = findViewById(R.id.helloAlarm);
-        mScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
-        this.sharedPreferences = PreferenceManager
-                .getDefaultSharedPreferences(AlarmActivity.this);
-        helloAlarm.setOnClickListener(new View.OnClickListener() {
+        init();
+        onClickListener();
+    }
+
+    private void onClickListener() {
+        alarmActive.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
-            public void onClick(View v) {
-                AlarmJobService.ringtone.stop();
-                finish();
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    AlarmJobService.ringtone.stop();
+                    finish();
+                }
             }
         });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void init() {
+        setContentView(R.layout.alarm_activity);
+        alarmIcon = findViewById(R.id.alarmIcon);
+        alarmActive = findViewById(R.id.alarm_active);
+        Animation animation = AnimationUtils.loadAnimation(AlarmActivity.this,
+                R.anim.shake);
+        alarmIcon.startAnimation(animation);
     }
 
     @Override
