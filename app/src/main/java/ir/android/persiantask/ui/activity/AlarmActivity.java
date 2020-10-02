ea4ee9192d1a92d526585aef8f176c8f8c279e53
ASUS <sara.mousavi.90@gmail.com>
2020-10-02 13:47:55 +0330

    improve alarm activity ui

--- app/src/main/java/ir/android/persiantask/ui/activity/AlarmActivity.java
+++ app/src/main/java/ir/android/persiantask/ui/activity/AlarmActivity.java
@@ -1,38 +1,28 @@
 package ir.android.persiantask.ui.activity;
 
-import android.app.job.JobInfo;
-import android.app.job.JobScheduler;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AnalogClock;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 
-import java.util.List;
-
 import ir.android.persiantask.R;
-import ir.android.persiantask.ui.activity.reminder.AddEditReminderActivity;
 import ir.android.persiantask.ui.services.AlarmJobService;
 
 public class AlarmActivity extends AppCompatActivity {
     private ImageView alarmIcon;
+    private AnalogClock alarmClockIcon;
     private SwitchCompat alarmActive;
+    private TextView alarmTitle;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -49,6 +39,7 @@ public class AlarmActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (!isChecked) {
                     AlarmJobService.ringtone.stop();
+                    alarmTitle.setVisibility(View.GONE);
                     finish();
                 }
             }
@@ -59,10 +50,13 @@ public class AlarmActivity extends AppCompatActivity {
     private void init() {
         setContentView(R.layout.alarm_activity);
         alarmIcon = findViewById(R.id.alarmIcon);
+        alarmClockIcon = findViewById(R.id.analogClock);
         alarmActive = findViewById(R.id.alarm_active);
+        alarmTitle = findViewById(R.id.alarmTitle);
         Animation animation = AnimationUtils.loadAnimation(AlarmActivity.this,
                 R.anim.shake);
         alarmIcon.startAnimation(animation);
+        alarmClockIcon.startAnimation(animation);
     }
 
     @Override
